262d97604201bc9481c2dba9bf60c5cbd33f25d3
kumarchidepudi <venkat.chidepudi@gmail.com>
2024-07-27 14:12:34 +0530

    Initial commit

---
+++ src/test/java/com/kumar/SampleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
